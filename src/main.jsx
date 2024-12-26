03d3f1f94ad71811fdc352af5fdcafdb84643cb3
Jenesyx <a.bidkhori2004@gmail.com>
2024-12-26 21:09:02 +0100

    Added Animations, lights for 3d model, added Sentry for monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+Sentry.init({
+  dsn: "https://e8bf8248b658426281dea78dbe26fe13@o4508529092788224.ingest.de.sentry.io/4508529094950992",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
